b1d612093141d837fb346f70a2e9fe6bab0c87ae
Tasneem Fathima <tasneem_fathima@epam.com>
2021-09-28 08:32:25 +0530

    comitting a text file

---
+++ arithmetic.java
@@ -0,0 +1,11 @@
+class add{
+	int addition(int a,int b){
+		return a+b;
+	}
+}
+
+public class arithmetic{
+	public sataic void main(String args[]){
+		add a=new add();
+		System.out.println(a.addition(1,2));
+}
